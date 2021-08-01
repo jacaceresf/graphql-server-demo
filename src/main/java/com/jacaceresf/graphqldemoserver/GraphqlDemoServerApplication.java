9623196f160f53cb0830f2e521e443f89bba6544
jorgecac <jorge97caceres@gmail.com>
2021-08-01 14:52:56 -0400

    Generating project.

---
+++ src/main/java/com/jacaceresf/graphqldemoserver/GraphqlDemoServerApplication.java
@@ -0,0 +1,13 @@
+package com.jacaceresf.graphqldemoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDemoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlDemoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacaceresf/graphqldemoserver/GraphqlDemoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacaceresf.graphqldemoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
